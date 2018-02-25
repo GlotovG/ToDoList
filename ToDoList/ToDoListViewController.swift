0af129b8d91cf1980dd1858b9afd2d79070da2db
Glotov Gennady <glotovg@gmail.com>
2018-02-25 23:28:00 +0300

    Добавлена новая функциональность - добавление новой записи для отображения на экране

--- ToDoList/ToDoListViewController.swift
+++ ToDoList/ToDoListViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ToDoListViewController: UITableViewController {
 
-    let toDoListArray = ["Купить молока", "Купить черный хлеб", "Купить яиц"]
+    var toDoListArray = ["Купить молока", "Купить черный хлеб", "Купить яиц"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -57,6 +57,55 @@ class ToDoListViewController: UITableViewController {
         
         //когда нажимаем на ячейку, она сереет и затем серость плавно снимается
         tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    //MARK - Добавление новой записи
+    //описываем дейсвтия при нажатии на "+" для добавления еще одной записи в ячейке на экарне
+    
+    @IBAction func AddButtonPressed(_ sender: Any) {
+        
+        //объявим переменную для сохранения текста из окна alert
+        var textField = UITextField()
+        
+        //объявление окна alert
+        let alert = UIAlertController(title: "Добавление новой записи", message: "", preferredStyle: .alert)
+        
+        //объявления кнопки для alert для добавления записи
+        let action = UIAlertAction(title: "Добавить запись", style: .default) { (action) in
+            // что происходит при нажатии кнопки "добавить запись" на UIAlert
+            print("Нажата кнопка добавить запись для записи - \(textField.text ?? "пустая строка")")
+            
+            if textField.text != "" {
+                //добавление новой записи в массив данных
+                self.toDoListArray.append(textField.text!)
+                
+                //обновляем окно для отображения новой добавленной записи польщователем
+                self.tableView.reloadData()
+            }
+        }
+        
+        //объявления кнопки для alert для отмены добавления записи
+        let actionCancel = UIAlertAction(title: "Отмена", style: .default) { (action) in
+            // что происходит при нажатии кнопки "добавить запись" на UIAlert
+            print("Добавление новой записи отменено пользователем!")
+        }
+        
+        //добавление тектоового поля к окну alert
+        alert.addTextField { (alertTextField) in
+            //значение до ввода текста в поле ввода
+            alertTextField.placeholder = "Укажи текст для добавления"
+            //присвоение переменной текста из поля ввода новой записи
+            textField = alertTextField
+        }
+        
+        //доабавляем в к окну alert действия action "добавить запись"
+        alert.addAction(action)
+        //доабавляем в к окну alert действия actionCancel "отмена"
+        alert.addAction(actionCancel)
+        
+        //отображаем alert пользователю с анимацией
+        present(alert, animated: true, completion: nil)
+        
     }
     
     
